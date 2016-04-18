3abc4291db9d7f41c60a7ba5b9bb7796a93c98f9
Philipp Steinpass <philipp@xndr.de>
2016-04-18 21:21:25 +0200

    fixed indentation.

--- default.nix
+++ default.nix
@@ -3,18 +3,18 @@ with import <nixpkgs> {};
   env = stdenv.mkDerivation {
     name = "TheAdventuresOfBlinky";
     buildInputs = [
-    	cmake
-	gcc
-    	git
-	less
-    	sfml
-        openssh
-	stdenv
-        vim
+      cmake
+      gcc
+      git
+      less
+      sfml
+      openssh
+      stdenv
+      vim
     ];
     SSL_CERT_FILE="/etc/ssl/certs/ca-bundle.crt";
-        shellHook = ''
-	      export PS1="TheAdventuresOfBlinky:[\W] "
-	          '';
-  };
-}
+    shellHook = ''
+    export PS1="TheAdventuresOfBlinky:[\W] "
+    '';
+    };
+    }
