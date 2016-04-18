1fd00b7f812b6d911470bf68c829a60994a2029f
Philipp Steinpass <philipp@xndr.de>
2016-04-18 21:19:16 +0200

    Added default.nix for nix package managers.

---
+++ default.nix
@@ -0,0 +1,20 @@
+with import <nixpkgs> {};
+{
+  env = stdenv.mkDerivation {
+    name = "TheAdventuresOfBlinky";
+    buildInputs = [
+    	cmake
+	gcc
+    	git
+	less
+    	sfml
+        openssh
+	stdenv
+        vim
+    ];
+    SSL_CERT_FILE="/etc/ssl/certs/ca-bundle.crt";
+        shellHook = ''
+	      export PS1="TheAdventuresOfBlinky:[\W] "
+	          '';
+  };
+}
